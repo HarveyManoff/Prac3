0e5dbea8732474a6960ed10768dc76d0ed9b544e
Harvey <harvey96@bigpond.com>
2017-03-17 17:33:23 +1000

    updated

---
+++ Temperatures.py
@@ -0,0 +1,37 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+__author__ = 'Lindsay Ward'
+
+MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+print(MENU)
+choice = input(">>> ").upper()
+
+
+def To_fahrenheit():
+    global celsius, fahrenheit
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+
+def To_Celcius():
+    global fahrenheit, celsius
+    fahrenheit = float(input("fahrenheit:  "))
+    celsius = (5 / 9) * (fahrenheit - 32)
+    print("Result: {:.2f} C".format(celsius))
+
+
+while choice != "Q":
+    if choice == "C":
+        To_fahrenheit()
+    elif choice == "F":
+        # TODO: Write this section so the program converts F to C and displays the result
+        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+        To_Celcius()
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
